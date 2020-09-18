ee9a172be75c403504a5ef11d2c88ac9c5298fd7
marwenabbassy <marwen.abbassy2021@gmail.com>
2020-09-19 01:56:07 +0200

    show more bouton

--- src/component/Liste.js
+++ src/component/Liste.js
@@ -9,22 +9,41 @@ import './Liste.css';
 
 const Liste = () => {
    const [userData,setUserData]=useState([]);
+ 
    useEffect(()=>{
        const fetchData=async()=>{
            const person=await axios
            .get('https://jsonplaceholder.typicode.com/users');
-           setUserData(person.data);
-
+           setUserData(person.data.map(el=>{return {...el,show:false}}));
+          
+         
        }
        fetchData();
+    
+       
    },[])
-   
+   const handleshow=(id)=>{
+    setUserData(userData.map(el=>el.id===id?{...el,show:!el.show}:{...el,show:el.show}))
+    
+      
+      console.log(userData)
+    //   for (let i=0;i<userData.length;i++){
+    //       if(userData[i].id===id){
+    //         userData[i].show=true         }else {
+    //         userData[i].show=false     }
+    //   }
+    // userData.map(el=>el.id===id?el.show=!sh)
+  
+    console.log(userData)
+
+
+   }
   
     return ( 
         <div className="app">
             <ul>
                 {userData.map((el)=>{
-                    return(<div className="card">
+                    return(<div className="card" key={el.id}>
                         <Card className="text-center">
                             <div >
                         <Card.Header ><h3> Name: {el.name}</h3></Card.Header>
@@ -36,9 +55,14 @@ const Liste = () => {
                            <span> Num Tel: {el.phone}
                            </span>
                            </h6>
+                           {console.log(el.show)}
+                           <p style={{display:(el.show?"block":"none")}}>
+                               adresse: {el.address.street}
+                           </p>
+                           
                            
                           </Card.Text>
-                          <Button className="button"  >
+                          <Button className="button" onClick={()=>handleshow(el.id)} >
                               <span>More information</span></Button>
                         </Card.Body>
                       </div>
